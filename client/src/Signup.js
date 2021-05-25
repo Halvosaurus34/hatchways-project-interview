c339cda507aba2db494a72b0f63e0e37e0437885
Levi <lhalvorson34@gmail.com>
2021-05-25 14:03:54 -0600

    styled login and signup using material ui

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -3,15 +3,16 @@ import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import {
   Grid,
-  Box,
   Typography,
   Button,
   FormControl,
   TextField,
   FormHelperText,
+  Box,
 } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
-
+import bubble from "./assets/bubble.svg";
+import backgroundImage from "./assets/bg-img.png";
 const Login = (props) => {
   const history = useHistory();
   const { user, register } = props;
@@ -37,16 +38,71 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
+    <Grid container style={{ height: "100vh" }}>
+      <Grid
+        container
+        item
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgb(58, 141, 255, 0.75), #86B9FF ), url(${backgroundImage})`,
+          opacity: "85%",
+
+          backgroundRepeat: "no-repeat",
+          backgroundColor: "linear-gradient(#3A8DFF, #86B9FF)",
+          backgroundSize: "cover",
+          width: "auto",
+        }}
+        sm={5}
+        md={5}
+        lg={5}
+        xl={5}
+        alignContent="center"
+        justify="center"
+      >
+        <img
+          src={bubble}
+          alt="chat icon"
+          style={{ maxWidth: "4rem", margin: "1rem 7rem" }}
+        />
+        <Typography
+          variant="h4"
+          style={{ color: "white", textAlign: "center", margin: "2rem" }}
+        >
+          Converse with anyone with any language
+        </Typography>
+      </Grid>
+      <Grid container item xs={12} sm={7} md={7} lg={7} xl={7} justify="center">
+        <Grid
+          container
+          item
+          justify="flex-end"
+          alignItems="baseline"
+          style={{ margin: "1rem" }}
+        >
+          <Typography variant="subtitle2" style={{ color: "#ccc" }}>
+            Already have an account?
+          </Typography>
+          <Button
+            onClick={() => history.push("/login")}
+            variant="text"
+            size="large"
+            style={{
+              height: "3.5rem",
+              boxShadow: "0 0  12px 2px rgb(231, 231, 231)",
+              margin: "1rem 1.5rem",
+              width: "10rem",
+            }}
+            color="primary"
+          >
+            Login
+          </Button>
         </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
+        <form onSubmit={handleRegister} style={{ width: "65%" }}>
+          <Grid justify="center" spacing={2}>
+            <Typography variant="h4" style={{ fontWeight: "700" }}>
+              Create an account.
+            </Typography>
+            <Grid style={{ marginTop: "1rem" }}>
+              <FormControl fullWidth={true}>
                 <TextField
                   aria-label="username"
                   label="Username"
@@ -56,8 +112,8 @@ const Login = (props) => {
                 />
               </FormControl>
             </Grid>
-            <Grid>
-              <FormControl>
+            <Grid style={{ marginTop: "1rem" }}>
+              <FormControl fullWidth={true}>
                 <TextField
                   label="E-mail address"
                   aria-label="e-mail address"
@@ -67,8 +123,11 @@ const Login = (props) => {
                 />
               </FormControl>
             </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
+            <Grid style={{ marginTop: "1rem" }}>
+              <FormControl
+                error={!!formErrorMessage.confirmPassword}
+                fullWidth={true}
+              >
                 <TextField
                   aria-label="password"
                   label="Password"
@@ -82,8 +141,11 @@ const Login = (props) => {
                 </FormHelperText>
               </FormControl>
             </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
+            <Grid style={{ marginTop: "1rem" }}>
+              <FormControl
+                error={!!formErrorMessage.confirmPassword}
+                fullWidth={true}
+              >
                 <TextField
                   label="Confirm Password"
                   aria-label="confirm password"
@@ -97,12 +159,25 @@ const Login = (props) => {
                 </FormHelperText>
               </FormControl>
             </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
+            <Box justify="center" style={{ display: "flex" }}>
+              <Button
+                type="submit"
+                variant="contained"
+                size="large"
+                color="primary"
+                style={{
+                  margin: "2rem auto",
+                  height: "3.5rem",
+                  borderRadius: 2,
+                  width: "40%",
+                }}
+              >
+                Create
+              </Button>
+            </Box>
           </Grid>
         </form>
-      </Box>
+      </Grid>
     </Grid>
   );
 };
