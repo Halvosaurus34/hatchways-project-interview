ad3ca34548413a17b5079d5d219c00c27ae1f23b
Levi <lhalvorson34@gmail.com>
2021-05-25 09:54:56 -0600

    changed the server/db/db.js file to work with my computer.  It wasnt pasing in any password for the database.

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,14 @@
 const Sequelize = require("sequelize");
-
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+require("dotenv").config();
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+//   {
+//     logging: false,
+//   }
+// );
+const db = new Sequelize("messenger", "postgres", process.env.PGPASSWORD, {
+  host: "localhost",
+  dialect: "postgres",
+  port: 5432,
 });
-
 module.exports = db;
